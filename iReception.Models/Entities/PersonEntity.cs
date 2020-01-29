4937f44af6ac8b95547cadb131dc63ef8f424a0b
Mateusz <mateuszkijewski2307@gmail.com>
2020-01-29 20:50:55 +0100

    Initial commit

---
+++ iReception.Models/Entities/PersonEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iReception.Models.Entities
+{
+    public class PersonEntity
+    {
+        public string thing;
+    }
+}
